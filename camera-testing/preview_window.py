02ea432686eb43f6f6d81769bda9c3d552b9d574
Reese Ford <reese_ford1@baylor.edu>
2024-12-03 10:58:03 -0600

    I don't know

--- camera-testing/preview_window.py
+++ camera-testing/preview_window.py
@@ -1,4 +1,7 @@
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-picam2.start_preview(Preview.QT)
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
