148a2ed58cc86ca3812d407fae603b22d8d8b55b
Reese Ford <reese_ford1@baylor.edu>
2024-12-05 12:19:57 -0600

    Some Pictures

--- camera-testing/take_picture.py
+++ camera-testing/take_picture.py
@@ -1,10 +1,14 @@
 from picamera2 import Picamera2
 import time
 from libcamera import controls
+import cv2
 
 picam2 = Picamera2()
 camera_config = picam2.create_still_configuration()
 picam2.configure(camera_config)
-picam2.set_controls({"ExposureTime": 20000, "AfMode": controls.AfModeEnum.Manual, "LensPosition": 2.0})
+picam2.set_controls({"ExposureTime": 10000, "AfMode": controls.AfModeEnum.Manual, "LensPosition": 2.0})
 picam2.start()
 picam2.capture_file("camera-testing/image.jpg")
+image = cv2.imread("camera-testing/image.jpg")
+cropped = image[0:2592, 463:4000]
+cv2.imwrite("camera-testing/cropped.jpg", cropped)
