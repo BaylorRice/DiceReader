eaa819943e07176589ddd44d878b0407e725b566
Reese Ford <reese_ford1@baylor.edu>
2024-12-10 12:01:51 -0600

    Audio is working

--- audio_files/test_audio.py
+++ audio_files/test_audio.py
@@ -4,7 +4,9 @@ import os
 os.system("amixer sset Master 70%")
 pygame.init()
 
-my_sound = pygame.mixer.Sound('audio-testing/dice_1.mp3')
+dice_number = 6
+
+my_sound = pygame.mixer.Sound('audio_files/dice_'+str(dice_number)+'.mp3')
 playing = my_sound.play()
 while playing.get_busy():
     pygame.time.delay(100)
--- dice_reader.py
+++ dice_reader.py
@@ -5,7 +5,7 @@ import pygame
 import os
 
 # Initilize PyGame
-os.system("amixer sset Master 50%")
+os.system("amixer sset Master 90%")
 pygame.init()
 
 class State(Enum):
@@ -17,7 +17,7 @@ class State(Enum):
 curr_state = State.WAITING_FOR_DICE
 
 def dice_detected():
-    face_detected = False
+    face_detected = True
     # Motion Detection Loop
     # Look for Dice
     return face_detected
@@ -25,25 +25,36 @@ def dice_detected():
 while True:
     if (curr_state == State.WAITING_FOR_DICE):
         dice_detect = dice_detected()
-        dice_detect = False
+
+        # State Change
         if dice_detect == True:
             curr_state = State.DICE_DETECTED
 
     elif (curr_state == State.DICE_DETECTED):
         # Use Model to Detect Number
         dice_number = None
+
+        # State Change
         if (dice_number != None):
             curr_state = State.SPEAK_NUMBER
 
     elif (curr_state == State.SPEAK_NUMBER):
-        # Pygame to speak number file
-        speak_done = False
-        if (speak_done == True):
+        # Play Sound File based on dice_number
+        my_sound = pygame.mixer.Sound('audio_files/dice_'+str(dice_number)+'.mp3')
+        playing = my_sound.play()
+        while playing.get_busy():
+            pygame.time.delay(100)
+        speaking = playing.get_busy()
+
+        # State Change
+        if (speaking == False):
             curr_state = State.WAITING_FOR_DICE_TO_LEAVE
 
     elif (curr_state == State.WAITING_FOR_DICE_TO_LEAVE):
         dice_detect = dice_detected()
-        dice_detect = True
+        dice_detect = False
+
+        # State Change
         if dice_detect == False:
             curr_state = State.WAITING_FOR_DICE
 
